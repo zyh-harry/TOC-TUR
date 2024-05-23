d2112f3205252fe748fac7f6ff7de3aad33c377b
Harry Zhang <169074763+zyh-harry@users.noreply.github.com>
2024-05-23 17:45:49 +0000

    Added empty columns for better data visualization in the combined eda file

--- feature_creation/combined_data_eda.py
+++ feature_creation/combined_data_eda.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-#Combined data files
 toc_file = "TOC_combined_with_discharge.csv"
 tur_file = "TUR_combined_with_discharge.csv"
 
@@ -8,23 +7,22 @@ tur_file = "TUR_combined_with_discharge.csv"
 toc_df = pd.read_csv(toc_file)
 tur_df = pd.read_csv(tur_file)
 
-#EDA Function
+#EDA function
 def perform_eda(df):
     eda_results = {}
     
-    #Statistics
+    #Summary statistics
     numeric_df = df.select_dtypes(include=['number'])
     eda_results['summary_statistics'] = numeric_df.describe().transpose()
     
-    #Any null values
+    #Any null values detected
     missing_values = df.isnull().sum().to_frame(name='missing_values')
 
-    #Calculates the % of missing values for each column, len(df) is the number of rows of the dataframe
-    missing_values['missing_percentage'] = (missing_values['missing_values'] / len(df)) * 100  
-    
+    #% of null values for each column
+    missing_values['missing_percentage'] = (missing_values['missing_values'] / len(df)) * 100
     eda_results['missing_values'] = missing_values
     
-    #Data correlation (matrix)
+    #Correlation matrix
     eda_results['correlation_matrix'] = numeric_df.corr()
     
     return eda_results
@@ -35,19 +33,33 @@ toc_eda = perform_eda(toc_df)
 #EDA on TUR
 tur_eda = perform_eda(tur_df)
 
-#EDA results
-toc_eda['summary_statistics'].to_csv("TOC_summary_statistics.csv")
-toc_eda['missing_values'].to_csv("TOC_missing_values.csv")
-toc_eda['correlation_matrix'].to_csv("TOC_correlation_matrix.csv")
+#EDA results saved separately
+toc_eda['summary_statistics'].to_csv('TOC_summary_statistics.csv')
+toc_eda['missing_values'].to_csv('TOC_missing_values.csv')
+toc_eda['correlation_matrix'].to_csv('TOC_correlation_matrix.csv')
 
-tur_eda['summary_statistics'].to_csv("TUR_summary_statistics.csv")
-tur_eda['missing_values'].to_csv("TUR_missing_values.csv")
-tur_eda['correlation_matrix'].to_csv("TUR_correlation_matrix.csv")
+tur_eda['summary_statistics'].to_csv('TUR_summary_statistics.csv')
+tur_eda['missing_values'].to_csv('TUR_missing_values.csv')
+tur_eda['correlation_matrix'].to_csv('TUR_correlation_matrix.csv')
 
-#EDA results in one CSV
-toc_eda_combined = pd.concat([toc_eda['summary_statistics'], toc_eda['missing_values'], toc_eda['correlation_matrix']], axis=1)
-toc_eda_combined.to_csv("TOC_eda.csv")
+#To separate sum stats, missing values and correlation matrix for better data visualization, blank columns are used
+def create_blank_df(rows):
+    return pd.DataFrame({'': [''] * rows})
 
-tur_eda_combined = pd.concat([tur_eda['summary_statistics'], tur_eda['missing_values'], tur_eda['correlation_matrix']], axis=1)
-tur_eda_combined.to_csv("TUR_eda.csv")
+#Combine all EDA results into one single dataframe with blank columns separating datasets in the final CSV
+def combine_eda_results(eda_results):
+    blank_df1 = create_blank_df(len(eda_results['summary_statistics']))
+    blank_df2 = create_blank_df(len(eda_results['missing_values']))
+    
+    combined_df = pd.concat([eda_results['summary_statistics'], blank_df1, 
+    eda_results['missing_values'], blank_df2, 
+    eda_results['correlation_matrix']], axis=1)
+    return combined_df
+
+toc_eda_combined = combine_eda_results(toc_eda)
+toc_eda_combined.to_csv('TOC_eda.csv', index=False)
+
+tur_eda_combined = combine_eda_results(tur_eda)
+tur_eda_combined.to_csv('TUR_eda.csv', index=False)
+
 
